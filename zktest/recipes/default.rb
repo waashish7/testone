4271c6905e253920fd90149709363b38ca96dbec
waashish7 <aashish.la@gmail.com>
2013-08-21 11:25:16 -0700

    Testing

---
+++ zktest/recipes/default.rb
@@ -0,0 +1,11 @@
+# Note that this is for the GB timezone. Look in
+# /usr/share/zoneinfo for your relevant file.
+service "vixie-cron"
+service "sysklogd"
+service "nginx"
+
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/GB"
+  notifies :restart, resources(:service => ["vixie-cron", "sysklogd", "nginx"]), :delayed
+  not_if "readlink /etc/localtime | grep -q 'GB$'"
+end
