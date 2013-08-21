84a5a60f60016a5ea6cd9dc41e0f2fa2f188fc71
waashish7 <aashish.la@gmail.com>
2013-08-21 13:48:23 -0700

    Testing again

--- zktest/recipes/default.rb
+++ zktest/recipes/default.rb
@@ -4,8 +4,15 @@
 # service "sysklogd"
 # service "nginx"
 
-link "/etc/localtime" do
-  to "/usr/share/zoneinfo/GB"
-  notifies :restart, resources(:service => ["vixie-cron", "sysklogd", "nginx"]), :delayed
-  not_if "readlink /etc/localtime | grep -q 'GB$'"
+#link "/etc/localtime" do
+#  to "/usr/share/zoneinfo/GB"
+#  notifies :restart, resources(:service => ["vixie-cron", "sysklogd", "nginx"]), :delayed
+#  not_if "readlink /etc/localtime | grep -q 'GB$'"
+#end
+
+
+link "/tmp/new" do
+    to "/tmp/old" 
+    link_type :symbolic
+    action :create
 end
